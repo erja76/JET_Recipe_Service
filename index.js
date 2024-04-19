5c0c497878d12f32431c9b7374683e8435658e86
tanja22011 <tanja22011@student.hamk.fi>
2024-04-19 09:17:17 +0300

    fetching recipes from api and saving it to db

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const axios = require('axios');
 const User = require('./models/user');
+const Recipe = require('./models/recipes');
 require('dotenv').config();
 
 const app = express();
@@ -30,6 +32,9 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.get('/admin', (req, res) => {
+  res.render('admin');
+});
 
 app.post('/register', (req, res) => {
     const formData = req.body;
@@ -54,6 +59,44 @@ app.post('/register', (req, res) => {
             res.status(500).send('Error registering user. Please try again.');
         });
 });
+
+
+// Fetching recipe from Edamem api
+app.post('/saverecipe', async (req, res) => {
+  try {
+    const { recipeName, recipeNumber } = req.body;
+
+    const response = await axios.get('https://api.edamam.com/search', {
+      params: {
+        q: recipeName,
+        app_id: process.env.EDAMAM_API_ID,
+        app_key: process.env.EDAMAM_API_KEY
+      }
+    });
+
+    const recipeFromApi = response.data.hits[recipeNumber].recipe;
+    //console.log('Response from Edamam API:', response.data);
+
+    // Save the recipe to the database
+    const savedRecipe = await Recipe.create({
+      name: recipeFromApi.label,
+      image: recipeFromApi.image,
+      ingredients: recipeFromApi.ingredientLines,
+      instruction: recipeFromApi.shareAs,
+      cusinetype: recipeFromApi.cuisineType,
+      mealtype: recipeFromApi.mealType,
+      dishtype: recipeFromApi.dishType,
+    });
+
+    res.json(savedRecipe);
+  } catch (error) {
+    console.error('Error fetching and saving recipe', error);
+    res.status(500).json({ error: 'Error fetching and saving recipe' });
+  }
+});
+
+
+
 
 module.exports = app;
 
