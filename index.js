41081094963bc90e7615713697b0544f6eb75fe6
Erja <erjapisila@yahoo.com>
2024-04-21 15:50:36 +0300

    working log in log out, admin

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
 const mongoose = require('mongoose');
 const axios = require('axios');
 const User = require('./models/user');
 const Recipe = require('./models/recipes');
 require('dotenv').config();
 const bcrypt = require('bcrypt');
+const passport = require('passport')
+const session = require('express-session')
+const LocalStrategy = require('passport-local').Strategy
+const flash = require('connect-flash');
+const routes = require('./routes');
 
 const app = express();
+
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+}))
 
 // Middleware
 app.engine('handlebars', exphbs.engine());
@@ -15,6 +27,57 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+// Custom Handlebars helper
+Handlebars.registerHelper('ifEqual', function (arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+// must add to list "= []", otherwise error: indexOf undefined 
+Handlebars.registerHelper('ifIn', function (elem, list = [], options) {
+    if (list.indexOf(elem) > -1) {
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
+
+// Passport Configuration
+authUser = async (username, password, done) => {
+    const user = await User.findOne({ email: username });
+    console.log(user);
+    // 1. If the user not found, done (null, false)
+    if (user === null) {
+        return done(null, false, { message: 'Incorrect email.' });
+        //      return done(null, false, req.flash('message', 'Incorrect email.'));
+    }
+    bcrypt.compare(password, user.password, (err, res) => {
+        if (err) { return done(err); }
+        if (res) {
+            // 3. If user found and password match, done (null, user)        
+            return done(null, user);
+        } else {
+            // 2. If the password does not match, done (null, false)
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+    });
+}
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, authUser));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id).lean();
+    done(null, user);
+});
+
+// Routes
+app.use('/', routes);
 
 // Database Connection
 const dbURI = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.CLUSTER}.mongodb.net/${process.env.DB}?retryWrites=true&w=majority&appName=Cluster0`;
@@ -28,115 +91,6 @@ mongoose.connect(dbURI)
     .catch((err) => {
         console.error('Error connecting to DB:', err);
     });
-
-// Routes
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-app.get('/admin', (req, res) => {
-    res.render('partials/admin');
-});
-
-app.get('/login', (req, res) => {
-    res.render('partials/login');
-});
-
-app.get('/register', (req, res) => {
-    res.render('partials/register');
-});
-
-app.get('/register_success', (req, res) => {
-    res.render('partials/register_success');
-});
-// Not yet in use
-app.post('/login', (req, res) => {
-    res.redirect('/recipe');
-});
-
-// Not yet in use
-//app.get('/recipe', (req, res) => {
-//    res.render('recipe');
-//});
-
-
-app.post('/register', (req, res) => {
-    const formData = req.body;
-    const password = formData.password;
-
-    // Create a new user
-    // Generating salt for hashing passwords
-    const saltRounds = 10; // The complexity of the hashing algorithm
-    bcrypt.hash(password, saltRounds, function (err, hash) {
-        if (err) {
-            console.error('Error hashing password:', err);
-            res.status(500).send('Error registering user. Please try again.');
-        } else {
-            const newUser = new User({
-                name: formData.name,
-                email: formData.email,
-                password: hash,
-                recipeInterests: formData.recipePreferences || [],
-                receiveRecommendations: formData.receiveRecommendations === 'true',
-                adminRights: false
-            });
-
-            // Save the new user to the database
-            newUser.save()
-                .then(() => {
-                    console.log('User registered successfully:', newUser);
-                    res.redirect('/register_success');
-                })
-                .catch((err) => {
-                    console.error('Error registering user:', err);
-                    res.status(500).send('Error registering user. Please try again.');
-                });
-        }
-    });
-});
-
-
-// Fetching recipe from Edamem api
-app.post('/saverecipe', async (req, res) => {
-    try {
-        const { recipeName, recipeNumber } = req.body;
-
-        const response = await axios.get('https://api.edamam.com/search', {
-            params: {
-                q: recipeName,
-                app_id: process.env.EDAMAM_API_ID,
-                app_key: process.env.EDAMAM_API_KEY
-            }
-        });
-        //        console.log(response.data.hits);
-        if (response.data.hits[recipeNumber] === undefined) {
-            res.status(400).send('No recipe found with given search terms.');
-        }
-        else {
-            const recipeFromApi = response.data.hits[recipeNumber].recipe;
-            console.log('Response from Edamam API:', response.data);
-            //            console.log(response.data.hits.length);
-
-            // Save the recipe to the database
-            const savedRecipe = await Recipe.create({
-                name: recipeFromApi.label,
-                image: recipeFromApi.image,
-                ingredients: recipeFromApi.ingredientLines,
-                instruction: recipeFromApi.shareAs,
-                cuisinetype: recipeFromApi.cuisineType,
-                mealtype: recipeFromApi.mealType,
-                dishtype: recipeFromApi.dishType,
-            });
-
-            res.json(savedRecipe);
-        }
-    } catch (error) {
-        console.error('Error fetching and saving recipe', error);
-        res.status(500).json({ error: 'Error fetching and saving recipe' });
-    }
-});
-
-
 
 
 module.exports = app;
